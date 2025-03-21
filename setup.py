7fcb308d58e70fb2e00eba06b764ac0328c6855b
Tiago Ribeiro Navarro de Andrade <42575083+tiagornandrade@users.noreply.github.com>
2025-03-21 17:29:00 -0300

    chore: adiciona arquivo setup

--- setup.py
+++ setup.py
@@ -1,9 +1,22 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="synthetics_data",
+    name="synthetics-data",
     version="0.1.0",
     packages=find_packages(where="src"),
     package_dir={"": "src"},
-    package_data={"commons": ["py.typed"]},
+    include_package_data=True,
+    package_data={
+        "commons": ["py.typed"],
+    },
+    install_requires=[
+        "faker",
+        "pydantic",
+        "pydantic[email]",
+        "numpy",
+        "pandas",
+        "pytest",
+        "pytest-cov",
+        "pytest-mock",
+    ],
 )
